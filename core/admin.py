405b09969774d0cdef2253d82e8502a58b785ca0
CJ <chiran.jeevi3013@gmail.com>
2025-04-14 23:51:45 -0700

    Updated Admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Team, Member, Event
 
-# Register your models here.
+admin.site.register(Team)
+admin.site.register(Member)
+admin.site.register(Event)
